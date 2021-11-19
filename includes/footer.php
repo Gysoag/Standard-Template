d69ada219e9bb284eed67379b60eb68157beeb47
Jeffrey Widmer <jeffrey.widmer@gyso.ch>
2021-11-19 15:26:06 +0100

    Standard Ausführung eingebunden. Bildpfad ist noch nicht richtig: kundenupload/content-management/img/gyso-logo.svg anstatt /kundenupload/content-management/img/gyso-logo.svg

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<?php
+$today = date('d.m.Y');
+$now = date('H:i:s');
+?>
+
+  <footer>
+    <p>Seite abgerufen am <?= $today ?> um <?= $now ?> Uhr.</p>
+  </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,176 @@
+<!-- Include Header -->
+<?php include './includes/header.php'; ?>
+
+<!-- Code -->
+<div class="content page-staticcontent">  
+    <section class="section-wrapper">
+        <div class="section-inner">
+
+            <div id="page-15853">
+
+                <!-- Netzwerk / Handelspartner -->
+                <div class="ma50">
+                    <div>
+                        <h1 class="font-c-b"><span class="font-w-s">Netzwerk /</span> Handelspartner</h1>
+                        <p class="ma15">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div class="grid ma30">                          
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Netzwerk / Verbände -->
+                <div class="ma50">
+                    <div>
+                        <h1 class="font-c-b"><span class="font-w-s">Netzwerk /</span> Verbände</h1>
+                        <p class="ma15">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div class="grid ma30">                          
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Netzwerk / Bildungspartner -->
+                <div class="ma50">
+                    <div>
+                        <h1 class="font-c-b"><span class="font-w-s">Netzwerk /</span> Bildungspartner</h1>
+                        <p class="ma15">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div class="grid ma30">                          
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="kundenupload/content-management/img/bostik.jpg" alt="">
+                            <div class="overlay">
+                                <a href="www.google.ch"><p class="text">Gebäudehülle <span class="text-light">Schweiz</span></p></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>             
+        </div>      
+    </section>
+</div>
+
+<!-- Include Footer -->
+<?php include './includes/footer.php'; ?>
+    
